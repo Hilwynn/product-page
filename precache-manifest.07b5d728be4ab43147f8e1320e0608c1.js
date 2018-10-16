5d5ec99d1675d079c939d8d05834afe714a7cdd4
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-16 16:39:08 +0200

    Updates

---
+++ precache-manifest.07b5d728be4ab43147f8e1320e0608c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf43d607cf40f38c45de",
+    "url": "/product-page/static/js/runtime~main.bf43d607.js"
+  },
+  {
+    "revision": "9dedadc4df12d44c0c65",
+    "url": "/product-page/static/js/main.9dedadc4.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/product-page/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "9dedadc4df12d44c0c65",
+    "url": "/product-page/static/css/main.d1989b79.chunk.css"
+  },
+  {
+    "revision": "85c061e0823f96531bea3c11216a43c2",
+    "url": "/product-page/index.html"
+  }
+];
